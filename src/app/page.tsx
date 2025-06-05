efb6530e323d018db83b184b36f15839d7c17347
Félix Herrmann <felix.herrmann@etu.unistra.fr>
2025-06-05 17:01:14 +0200

    Added some projects to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,7 @@ export default function Page() {
       <section id="education">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 7}>
-            <h2 className="text-xl font-bold">Parcours universitaire</h2>
+            <h2 className="text-xl font-bold">Formation</h2>
           </BlurFade>
           {DATA.education.map((education, id) => (
             <BlurFade
@@ -120,12 +120,10 @@ export default function Page() {
                   Mes projets
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Check out my latest work
+                  
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple
-                  websites to complex web applications. Here are a few of my
-                  favorites.
+                  Voici une liste de différents projets sur lesquels j'ai pu travaillé au cours de mes études.
                 </p>
               </div>
             </div>
